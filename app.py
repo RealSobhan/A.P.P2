83117458518184082f37661057319d650ec39f88
Sobhan <Sobhan.kasa@gmail.com>
2023-06-05 22:08:49 +0330

    connect app to config

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_session import Session
 from config import Config
 
 # create the base of the back
 app = Flask(__name__)
+app.config.from_object(Config)
+Session(app)
 
 
 if __name__ == '__main__':
