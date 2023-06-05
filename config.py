42907a8cd37bcc147f36351f0c8cbe7083289ab8
Sobhan <Sobhan.kasa@gmail.com>
2023-06-05 22:09:06 +0330

    more of config

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import secrets
 
 #setting configs that is needed to run the app
 class Config:
+    SESSION_TYPE = 'filesystem'
     SECRET_KEY = secrets.token_hex(16)
-
+    SESSION_FILE_DIR = './.flask_session/'
