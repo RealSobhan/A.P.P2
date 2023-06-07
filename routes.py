c844a7c88d95ce26c1f87e0489ac58ba850aff44
Ali Bozorgmehry <bozorgmehry.ali@gmail.com>
2023-06-07 16:07:34 +0330

    add and remove routes for cart and shop

--- routes.py
+++ routes.py
@@ -21,3 +21,60 @@ def index():
     for product in product_to_show:
         product["price"] = float(product["price"])
     return render_template('products.html', products=product_to_show, exchange_rate=int(get_currency() * 10), title="Home")
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    global products  # Declare the variable as global
+
+    if request.method == 'POST':
+        new_row = pd.DataFrame({'name': [request.form['name']], "price": [float(request.form['price'])], "drive_link": [correct_link(request.form['image'])]})
+        products = pd.concat([products, new_row], ignore_index=True)
+        products.to_csv('warehouse.csv', index=False)
+        products = pd.read_csv("warehouse.csv")
+
+    return render_template('admin.html') 
+
+@app.route('/remove_from_warehouse', methods=['GET', 'POST'])
+def remove_from_warehouse():
+    global products  # Declare the variable as global
+
+    if request.method == 'POST':
+        products = products[products["name"] != request.form['name']]
+        products.to_csv('warehouse.csv', index=False)
+        products = pd.read_csv("warehouse.csv")
+    return render_template('admin.html') 
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    name = request.form['name']
+    price = float(request.form['price'])
+    image = request.form['image']
+    product = {'name': name, 'price': price, 'image': image}
+    
+    if 'cart' not in session:
+        session['cart'] = []
+    session['cart'].append(product)
+    session.modified = True  # Save the session after modifying the cart
+    
+    return redirect(url_for('cart'))
+
+
+@app.route('/remove_from_cart', methods=['POST'])
+def remove_from_cart():
+    name = request.form['name']
+    
+    cart_items = session.get('cart', [])
+    new_cart = []
+    
+    for item in cart_items :
+        if item['name'] != name:
+            new_cart.append(item)
+        else:
+            continue
+        
+    cart_items = new_cart
+    
+    session['cart'] = cart_items
+    session.modified = True  # Save the session after modifying the cart
+    
+    return redirect(url_for('cart'))
